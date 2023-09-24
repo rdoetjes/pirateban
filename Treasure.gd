f53bf7e5999dc4c359d4da869221ab16df97921d
Raymond Doetjes <rdoetjes@phonax.com>
2023-09-24 09:02:39 +0200

    Added door and treasure

--- Treasure.gd
+++ Treasure.gd
@@ -9,7 +9,7 @@ func _ready():
 	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func set_grid_position(new_pos : Vector2i, use_tween: bool) -> void:
